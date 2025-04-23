24a1de330ebabff48786d2289da32ba135247e46
sakibraihan <sakibraihan47@gmail.com>
2025-04-23 12:40:22 +1000

    Added Banner on Front Homepage

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,8 +6,8 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    university: { type: String },
-    address: { type: String },
+    //university: { type: String },
+    //address: { type: String },
 });
 
 userSchema.pre('save', async function (next) {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Blogs from './pages/Blogs';
+import Home from './pages/Home';
 import ViewBlogs from './pages/ViewBlogs';
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+      <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
